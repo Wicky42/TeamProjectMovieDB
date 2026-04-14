e1c5d8beb5cbeba630be26923f78325bcc7fb031
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-04-14 16:24:26 +0200

    add Authent. Bearer

--- backend/src/main/java/org/example/backend/client/OpenAiClient.java
+++ backend/src/main/java/org/example/backend/client/OpenAiClient.java
@@ -1,5 +1,7 @@
 package org.example.backend.client;
 
+import org.example.backend.dto.OpenAIRequest;
+import org.example.backend.dto.OpenAIResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -15,6 +17,14 @@ public class OpenAiClient {
         this.apiKey = apiKey;
         this.restClient = RestClient.builder()
                 .baseUrl(BASE_URL)
+                .defaultHeader("Authorization", "Bearer " + apiKey)
                 .build();
+    }
+
+    public OpenAIResponse findMovieImdbID_whenCalledWithPrompt(String prompt){
+        return restClient.post()
+                .body(new OpenAIRequest("Finde einen Film der zu dieser Suche passt: " + prompt + ". Gib nur eine gültige imdbID zurück (Regex: tt\\\\d+). Keine Erklärung."))
+                .retrieve()
+                .body(OpenAIResponse.class);
     }
 }
