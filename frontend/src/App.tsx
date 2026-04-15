f79884bbad312826e1710a876b8b41d8adc6392c
ChrisClsg <chris@clsg.de>
2026-04-15 13:29:53 +0200

    Implement base style, header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import './styles/index.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="app-shell">
+      <Header />
+      <main className="app-shell__content">
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
