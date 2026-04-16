26fdece85aa6bca07e3ccfeb643f05053beec0e2
ChrisClsg <chris@clsg.de>
2026-04-16 15:14:12 +0200

    Introduce WatchlistEntryRepo and define mongo document for WatchlistEntry

--- backend/src/main/java/org/example/backend/domain/WatchlistEntry.java
+++ backend/src/main/java/org/example/backend/domain/WatchlistEntry.java
@@ -1,7 +1,9 @@
 package org.example.backend.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("watchlist_entries")
 public record WatchlistEntry(
   @Id
   String id,
